e2366dfdb7c781fb88155197a8fabb41d2c2e326
BCooperF <bradley_faesy@mymail.eku.edu>
2026-04-07 11:25:29 -0400

    04/07 update table View

--- M9TabelView/ViewController.swift
+++ M9TabelView/ViewController.swift
@@ -4,38 +4,93 @@
 //
 //  Created by Faesy, Bradley C. on 4/2/26.
 
-
-//  indexPath.Cell
-//  indexPath.Section
-//  Dalegate Pattern: One object allows another object to handle some of it's task
-//  1.) Datat Source: Get data
-//  2.) Delegate: Responce to user actions
+//indexPath.cell
+//indexPath.section
+//Delegate Pattern:
+//one object allows another object to handle some of its task
+//1) Datasource: get data
+//2) Delegate: reponse to user actions
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		print ("#1.) ", #function)
-		return 100
-	}
-	// Called repeatedly
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		print ("#1.) ", #function, indexPath)
-		
-		// #1.) Get or reuse a cell
-		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-		
-		// #2.) Put data into the cell
-		if indexPath.row < 19 {
-			cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
-		}
-		
-		// #3.)Return the cell
-		return cell
-	}
+class ViewController: UIViewController {
+	let fruits = ["Apple", "Banana", "Strawberry", "Mango", "Orange"]
+	let vegetables = ["Carrot", "Tomato", "Broccoli", "Cabbage"]
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+}
+
+extension ViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		switch indexPath.section {
+		case 0:
+			print("You selected \(fruits[indexPath.row])")
+		case 1:
+			print("You selected \(vegetables[indexPath.row])")
+		default:
+			break
+		}
+	}
+}
+
+extension ViewController: UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 2
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		print("#1.",#function)
+		switch section {
+		case 0:
+			return fruits.count
+		case 1:
+			return vegetables.count
+		default:
+			return 0
+		}
+	}
+	//This method is called repeatedly
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		print("#1.", #function, indexPath)
+		//#1. Get or reuse a cell
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		//#2. Put data into the cell
+		switch indexPath.section {
+		case 0:
+			cell.textLabel?.text = fruits[indexPath.row]
+		case 1:
+			cell.textLabel?.text = vegetables[indexPath.row]
+		default:
+			break
+		}
+		
+		//#3. Return the cell
+		return cell //for a cell
+	}
+	
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		switch section {
+		case 0:
+			return "Fruits"
+		case 1:
+			return "Vegetables"
+		default:
+			return nil
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+		switch section {
+		case 0:
+			return "\(fruits.count) fruits are good"
+		case 1:
+			return "\(vegetables.count) vegetables are good"
+		default:
+			return nil
+		}
+
 	}
 }
